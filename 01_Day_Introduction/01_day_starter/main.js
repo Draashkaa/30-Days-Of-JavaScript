a9dd8fbea481734c2ef631daf5c5454609bb904d
Timur <danilov.tim@inbox.ru>
2024-10-27 14:11:13 +0600

    [+] FullDay

--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -1,4 +1,27 @@
-// the variable values can be accessed from other variable.js file
-console.log(firstName, lastName, country, city, age, isMarried) 
-console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
-console.log(name, job, live)
+// 1 comments can make code readable
+// 2 Welcome to 30DaysOfJavaScript
+/* 3
+ comments can make code readable, easy to reuse and informative
+*/
+//6
+let e
+let f
+let g
+//7
+let h = 1
+let i = 2
+let j = 3
+let k = 4
+//8
+let firstName1 = 'Asabeneh' 
+let lastName1 = 'Yetayeh' 
+let country1 = 'Finland' 
+let age1 = 100 
+let isMarried1 = true
+//9
+let firstName2 = 'Asabeneh', lastName2 = 'Yetayeh', country2 = 'Finland' ,age2 = 100 , isMarried2 = true 
+//10
+let myAge = 1337
+let yourAge = 228
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
